5e92134fc3eae3d15edecbf74aebef941b154f44
Hasan <aamirkhan2009@hotmail.com>
2021-11-09 20:51:04 +0530

    Connected app to database using mongoose

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 var express = require("express")
+var mongoose = require("mongoose")
 var app = express()
 
 app.use(express.static(__dirname));
+const username = 'dbUser';
+const password = 'YDaGUIbcBzyxcRjT';
+const cluster = 'cluster0.rwc6i'
+const dbname = 'chat-app';
+
+
+mongoose.connect(
+    `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 var server = app.listen(3000, () => {
     console.log("Running on port", server.address().port)

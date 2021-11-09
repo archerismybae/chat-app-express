63b3f80e2a981ddc4d43dfb660a0dffc05fd453a
Hasan <aamirkhan2009@hotmail.com>
2021-11-09 22:46:52 +0530

    Added routes for sending and displaying messages

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var express = require("express")
 var mongoose = require("mongoose")
 var app = express()
+//var bodyParser = require("body-parser")
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 app.use(express.static(__dirname));
+
+var Message = mongoose.model('Messages',{ name : String, message : String});
+
 const username = 'dbUser';
 const password = 'YDaGUIbcBzyxcRjT';
 const cluster = 'cluster0.rwc6i'
 const dbname = 'chat-app';
-
 
 mongoose.connect(
     `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`,
@@ -22,6 +27,22 @@ db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
   console.log("Connected successfully");
 });
+
+app.get('/messages', (req, res) => {
+    Message.find({}, (err, messages) => {
+        res.send(messages);
+    })
+})
+
+app.post('/messages', (req, res) => {
+    var message = new Message(req.body);
+    message.save((err) => {
+        if (err) {
+            res.sendStatus(500);
+        }
+        res.sendStatus(200);
+    })
+})
 
 var server = app.listen(3000, () => {
     console.log("Running on port", server.address().port)
